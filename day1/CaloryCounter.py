09ed201a0a0efaee9085a32dd717700c4edcee70
Thomas Rau <thomas.rau@ul.com>
2022-12-01 10:21:11 +0100

    Day 1 Part 2 Implemented
    Methods added to identify top 3 elves with most calories and their total sum of calories.

--- day1/CaloryCounter.py
+++ day1/CaloryCounter.py
@@ -33,13 +33,36 @@ def find_elf_with_most_calories(elves_calories):
     elf = numpy.argmax(elves_calories)
     return elf, elves_calories[elf]
 
+
+def find_n_most_calrories_elves(elves_calories, n):
+    return (-elves_calories).argsort()[:n]
+
+
+def sum_special_elves_calories(elves_calories, special_elves_list):
+    sum = int(0)
+    for elves in special_elves_list:
+        sum += elves_calories[elves]
+    return sum
+
+
+def compute_top3_total_calories(elves_calories):
+    top3 = find_n_most_calrories_elves(elves_calories, 3)
+    return sum_special_elves_calories(elves_calories, top3)
+
+
 # Defining main function
 def main():
     elves_food_list_file = str(sys.argv[1])
     elves_food_list = read_txt_list(elves_food_list_file)
+    
     elves_calories = compute_elves_calories(elves_food_list)
+    
     max_calory_elf, max_calories = find_elf_with_most_calories(elves_calories)
     print("Elf number ", max_calory_elf, "with ", max_calories, " calories")
+    
+    top3_total_calories = compute_top3_total_calories(elves_calories)
+    print("Top 3 elves total calories: ", top3_total_calories) 
+
     
 
 # main
