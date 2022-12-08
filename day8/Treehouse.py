c5d5fe24f50056e4a6e78bc2959ae713d0a6c3b2
Thomas Rau <thomas.rau@ul.com>
2022-12-08 15:28:51 +0100

    Day 8 Working
    Brut force, there is for sure a nicer way

--- day8/Treehouse.py
+++ day8/Treehouse.py
@@ -12,7 +12,7 @@ def print_LOL(list_of_list:list[list]):
 
 
 def test_tree_visibility(trees:list[list], i: int, j: int):
-     #up 
+    #up 
     highest = -1
     for k in range(i-1, -1, -1):
         if trees[k][j] > highest:
@@ -51,6 +51,54 @@ def test_tree_visibility(trees:list[list], i: int, j: int):
     return False
 
 
+def compute_scenic_score(trees:list[list], i: int, j: int):
+    scenic_score = 1
+
+    #up 
+    nr_trees = 0
+    for k in range(i-1, -1, -1):
+        nr_trees += 1
+        if trees[i][j] <= trees[k][j]:
+            break
+    scenic_score *= nr_trees 
+
+    #down
+    nr_trees = 0
+    for k in range(i+1, len(trees)):
+        nr_trees += 1
+        if trees[i][j] <= trees[k][j]:
+            break
+    scenic_score *= nr_trees 
+
+    #left 
+    nr_trees = 0
+    for k in range(j-1, -1, -1):
+        nr_trees += 1
+        if trees[i][j] <= trees[i][k]:
+            break
+    scenic_score *= nr_trees 
+
+    #right
+    nr_trees = 0
+    for k in range(j+1, len(trees)):
+        nr_trees += 1
+        if trees[i][j] <= trees[i][k]:
+            break
+    scenic_score *= nr_trees 
+    return scenic_score
+
+
+def compute_max_scenic_score(trees:list[list]):
+    max_score = 0
+    scenic_score = 0
+    for i in range(len(trees)):
+        for j in range(len(trees[i])):
+            scenic_score = compute_scenic_score(trees, i, j)
+            if scenic_score > max_score:
+                max_score = scenic_score
+    return max_score
+
+
 def test_visibility(trees:list[list]):
 
     nr_lines = trees.shape[0]
@@ -61,6 +109,7 @@ def test_visibility(trees:list[list]):
         for j in range(nr_cols):
             visible_trees[i][j] = test_tree_visibility(trees,i,j)
     return visible_trees
+
 
 def count_visible_trees(visible_trees):
     count = 0
@@ -75,12 +124,17 @@ def main():
     
     tree_file = str(sys.argv[1])
     trees = read_txt_matrix(tree_file)
+    #print_LOL(trees)
 
     visible_trees = test_visibility(trees)
     nr_visible_trees = count_visible_trees(visible_trees)
 
     print("There are ", nr_visible_trees, " trees visible.")
 
+    max_scenic_score = compute_max_scenic_score(trees)
+    print("Maximum Scenic Score is ", max_scenic_score)
+
+    print()
 
 
 # main
